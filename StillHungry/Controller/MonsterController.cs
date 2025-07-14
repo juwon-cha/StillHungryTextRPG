bd73a121da207740d048b649d50ea4c3870dbe6f
Juwon Cha <littlebigcha@gmail.com>
2025-07-14 20:25:15 +0900

    [ADD] 몬스터 데이터 추가

--- StillHungry/Controller/MonsterController.cs
+++ StillHungry/Controller/MonsterController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace StillHungry.Controller
 {
+    public enum EMonsterType
+    {
+        SLIME,
+        GOBLIN,
+        MIMICK,
+    }
+
     class MonsterController : CharacterController
     {
         // TODO: 몬스터 추가
