5c6b2828634503110af62292ad62ccd7187c7188
rootinge <jhpark5129@gmail.com>
2025-07-16 09:52:27 +0900

    [FIX] 매안화면 휴식 삭제 및 저장 추가

--- StillHungry/Scene/TownScene.cs
+++ StillHungry/Scene/TownScene.cs
@@ -6,7 +6,7 @@ namespace StillHungry.Scene
 {
     public class TownScene : BaseScene
     {
-        private readonly string[] mMenuItems = { "1. 상태 보기", "2. 인벤토리", "3. 길드", "4. 던전 입장", "5. 휴식하기", "0. 게임 종료" };
+        private readonly string[] mMenuItems = { "1. 상태 보기", "2. 인벤토리", "3. 길드", "4. 던전 입장","5. 저장하기", "0. 게임 종료" };
         private readonly IExecutable[] mMenuCommands;
         private readonly MenuNavigator mNavigator;
 
@@ -19,7 +19,6 @@ namespace StillHungry.Scene
                 new ChangeSceneCommand(ESceneType.INVENTORY_SCENE),
                 new ChangeSceneCommand(ESceneType.GUILD_SCENE),
                 new ChangeSceneCommand(ESceneType.DUNGEON_SCENE),
-                new ChangeSceneCommand(ESceneType.CAMPSITE_SCENE),
                 new SaveGameCommand(),
                 new ExitGameCommand()
             };
