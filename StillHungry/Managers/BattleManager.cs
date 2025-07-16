be87b775dcfb0d3ac34334ec4a32d4353712e1b6
유경 <saa1007@naver.com>
2025-07-16 11:02:01 +0900

    ??

--- StillHungry/Managers/BattleManager.cs
+++ StillHungry/Managers/BattleManager.cs
@@ -7,12 +7,11 @@ namespace StillHungry.Managers
 {
     public class BattleManager
     {
-        public int monsterKillCount = 0;
-
         //public PlayerController PlayerController = new PlayerController();
         public MonsterController MonsterController = new MonsterController();
         public bool isFighting = false;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         public int initialHP; //전투 시작 시의 플레이어 체력
         public int totalDamageTaken = 0; //전투 중 받은 누적 피해
@@ -25,57 +24,11 @@ namespace StillHungry.Managers
         public MonsterAction LastAction { get; private set; }
 
 >>>>>>> develop
+=======
+>>>>>>> parent of 91e2d2e (전투결과 구현(확인못함))
         public BattleManager() 
         {
-            if (MonsterController.ActiveMonsters.Count == 0) 
-                MonsterController.SpawnMonstersForBattles(); 
-        }
-
-        public void startBattle() //전투 시작 시 호출될 함수
-        {
-            isFighting = true;
-            initialHP = Manager.Instance.Game.PlayerController.HP;
-            totalDamageTaken = 0;
-        }
-        public void EndBattle(bool isVictory, int initialHP, int damageTaken, int monsterKillCount)
-        {
-            var player = Manager.Instance.Game.PlayerController;
-
-            Console.Clear();
-            Console.WriteLine("Battle!! - Result\n");
-
-            if (isVictory)
-            {
-                Console.WriteLine("Victory\n");
-                Console.WriteLine($"던전에서 몬스터 {monsterKillCount}마리를 잡았습니다.");
-                Console.WriteLine($"Lv.{player.Level} {player.Name}");
-                Console.WriteLine($"HP {initialHP} -> {player.HP}\n");
-                Console.WriteLine("0. 다음");
-                Console.Write("\n>>");
-                string input = Console.ReadLine();
-
-                if (input == "0")
-                {
-                    isFighting = false;
-                    Manager.Instance.Scene.ChangeScene(ESceneType.TOWN_SCENE);
-                }
-            }
-            else
-            {
-                Console.WriteLine("You Lose\n");
-                Console.WriteLine($"Lv.{player.Level} {player.Name}");
-                Console.WriteLine($"HP {initialHP} -> {player.HP}\n");
-                Console.WriteLine("0. 다음");
-                Console.Write("\n>>");
-                string input = Console.ReadLine();
-
-                if (input == "0")
-                {
-                    Console.WriteLine("\n게임 종료");
-                    Thread.Sleep(1000);
-                    Environment.Exit(0);
-                }
-            }
+            if (MonsterController.ActiveMonsters.Count == 0) MonsterController.SpawnMonstersForBattles(); 
         }
 
         public void StartMonsterPhase()
@@ -137,4 +90,3 @@ namespace StillHungry.Managers
         }
     }
 }
-
